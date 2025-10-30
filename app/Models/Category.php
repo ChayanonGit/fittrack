a596037acd447bccb29669982a4d668609e41f72
ChayanonGit <chayanonappsheet2@gmail.com>
2025-10-30 13:04:02 +0700

    initial commit create db table and model

---
+++ app/Models/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    // หมวดหมู่มีหลายสินค้า
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    // หมวดหมู่มีหลายคอร์สฟิตเนส
+    public function fitnessCourses()
+    {
+        return $this->hasMany(FitnessCourse::class);
+    }
+}
---
+++ app/Models/OrderDetail.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderDetail extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['quantity', 'price'];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function course()
+    {
+        return $this->belongsTo(FitnessCourse::class, 'course_id');
+    }
+}
