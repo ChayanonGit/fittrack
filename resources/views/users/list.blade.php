71a48e8e17f1322e875a33919c60a0f13589c7ec
ChayanonGit <chayanonappsheet2@gmail.com>
2025-11-03 13:40:54 +0700

    user

--- resources/views/users/list.blade.php
+++ resources/views/users/list.blade.php
@@ -1,67 +1,72 @@
 @extends('users.main')
 
 @section('header')
-<search>
-    <form action="" method="get" class="app-cmp-search-form">
-        <div class="app-cmp-form-detail">
-            <label for="app-criteria-term">Search</label>
-            <input type="text" id="app-criteria-term" name="term" value="{{$criteria['term'] }}" />
+    <link rel="stylesheet" href="{{ asset('css/user.css') }}">
+    <div class="app-header-bar">
+        <div class="app-cmp-links-bar">
+            <nav>
+                <ul class="app-cmp-links" style="list-style: none; padding: 0; margin: 0;">
+                    <li>
+                        <a href="{{ route('users.create-form') }}" class="btn-cta">New User</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
 
-        <div class="app-cmp-form-actions">
-            <button type="submit" class="primary">Search</button>
-            <a href="{{route('users.list')}}">
-                <button type="button" class="accent">X</button>
-            </a>
-        </div>
-    </form>
-</search>
+        <!-- ฝั่งขวา -->
+        <div class="intro-right">
+            <search>
+                <form action="" method="get" class="app-cmp-search-form"
+                    style="display: flex; align-items: center; gap: 10px;">
+                    <div class="app-cmp-form-detail">
+                        <label for="app-criteria-term">Search</label>
+                        <input type="text" id="app-criteria-term" name="term" value="{{ $criteria['term'] }}" />
+                    </div>
 
-<div class="app-cmp-links-bar" style="max-width:1100px;margin:0 auto;">
-	<nav>
-		<ul class="app-cmp-links" style="list-style:none;padding:0;">
-			<li>
-				<a href="{{ route('users.create-form') }}" class="btn-cta">New User</a>
-			</li>
-		</ul>
-	</nav>
-	{{-- {{ $users->withQueryString()->links() }} --}}
-</div>
+                    <div class="app-cmp-form-actions">
+                        <button type="submit" class="primary">Search</button>
+                        <a href="{{ route('users.list') }}">
+                            <button type="button" class="accent">X</button>
+                        </a>
+                    </div>
+                </form>
+            </search>
+        </div>
+    </div>
 @endsection
 
 @section('content')
-<div style="max-width:1100px;margin:0 auto;">
-	<table class="app-cmp-data-list">
-		<colgroup>
-			<col style="width: 30ch;" />
-		</colgroup>
+    <div style="max-width:1100px;margin:0 auto;">
+        <table class="app-cmp-data-list">
+            <colgroup>
+                <col style="width: 30ch;" />
+            </colgroup>
 
-		<thead>
-			<tr>
-				<th>Email</th>
-				<th>Name</th>
-				<th>Role</th>
-			</tr>
-		</thead>
+            <thead>
+                <tr>
+                    <th>Email</th>
+                    <th>Name</th>
+                    <th>Role</th>
+                </tr>
+            </thead>
 
-		<tbody>
-			{{-- @php
+            <tbody>
+                {{-- @php
         session()->put('bookmarks.users.view',url()->full());
         session()->put('bookmarks.users.view-selves',url()->full());
 
         @endphp --}}
-			@foreach($users as $user)
-			<tr>
-				<td>
-					<a
-						href="{{ route('users.view', ['user' => $user->email]) }}"
-						class="app-cl-code">{{ $user->email }}</a>
-				</td>
-				<td>{{ $user->name }}</td>
-				<td>{{ $user->role }}</td>
-			</tr>
-			@endforeach
-		</tbody>
-	</table>
-</div>
+                @foreach ($users as $user)
+                    <tr>
+                        <td>
+                            <a href="{{ route('users.view', ['user' => $user->email]) }}"
+                                class="app-cl-code">{{ $user->email }}</a>
+                        </td>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->role }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
